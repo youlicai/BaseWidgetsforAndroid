bd0df2792c4f7f4a5b06c5a85eab3a4473493180
youlicai <youlicai@outlook.com>
2017-08-31 18:31:14 +0800

    add switchview

---
+++ XSwitchView.java
@@ -0,0 +1,20 @@
+package cn.haodian.demowidget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by 立才 on 2017/8/25.
+ */
+
+public class XSwitchView extends LinearLayout {
+
+    public XSwitchView(Context context) {
+        super(context);
+    }
+
+    public XSwitchView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
